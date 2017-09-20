de0a198aad387c0c92d511d2acae6f63193ad10b
Michael Herman <hermanmu@gmail.com>
2017-09-20 12:21:56 -0600

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Code Evaluation with AWS Lambda and API Gateway
+
+https://realpython.github.io/aws-lambda-code-execute/
---
+++ assets/main.js
@@ -0,0 +1,28 @@
+// globals
+const editor = ace.edit('editor');
+
+$(function() {
+  // configure ace editor
+  editor.setTheme("ace/theme/monokai");
+  editor.getSession().setMode("ace/mode/python");
+  editor.setFontSize('14px');
+});
+
+// handle form submit
+$('form').on('submit', (event) => {
+  event.preventDefault();
+  const answer = editor.getSession().getValue();
+  const payload = { answer: answer };
+  grade(payload);
+});
+
+// ajax request
+function grade(payload) {
+  $.ajax({
+    method: 'POST',
+    url: 'tbd',
+    data: payload
+  })
+  .done((res) => { console.log(res); })
+  .catch((err) => { console.log(err); });
+}
