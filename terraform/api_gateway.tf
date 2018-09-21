5b5ef61a7868cafbc405d054e9c98427452a5400
Jan Kogut <mr.jkogut@gmail.com>
2018-09-21 14:33:11 +0200

    Api_gateway.tf: add terraform config for API Gateway

---
+++ terraform/api_gateway.tf
@@ -0,0 +1,69 @@
+## API Gateway config
+
+resource "aws_api_gateway_rest_api" "PyAPI" {
+  name        = "TfServerlessPythonAPI"
+  description = "API for Serverless Python Application Example"
+  }
+
+resource "aws_api_gateway_resource" "ExecutePyResource" {
+  rest_api_id = "${aws_api_gateway_rest_api.PyAPI.id}"
+  parent_id   = "${aws_api_gateway_rest_api.PyAPI.root_resource_id}"
+  path_part   = "pyexecute"
+  }
+
+resource "aws_api_gateway_method" "ExecutePyMethod" {
+  rest_api_id   = "${aws_api_gateway_rest_api.PyAPI.id}"
+  resource_id   = "${aws_api_gateway_resource.ExecutePyResource.id}"
+  http_method   = "POST"
+  authorization = "NONE"
+  }
+
+## Lambda Integration  
+resource "aws_api_gateway_integration" "LambdaIntegration" {
+  rest_api_id = "${aws_api_gateway_rest_api.PyAPI.id}"
+  resource_id = "${aws_api_gateway_resource.ExecutePyResource.id}"
+  http_method = "${aws_api_gateway_method.ExecutePyMethod.http_method}"
+
+  integration_http_method = "POST"
+  type                    = "AWS"
+  uri                     = "${aws_lambda_function.PyLambdaFunction.invoke_arn}"
+  }
+
+ 
+## API DEPLOYMENT
+resource "aws_api_gateway_deployment" "PyAPIDeployment" {
+  depends_on = [
+    "aws_api_gateway_integration.LambdaIntegration"
+    ]
+
+  rest_api_id = "${aws_api_gateway_rest_api.PyAPI.id}"
+  stage_name  = "v1"
+  }
+
+## Permission for API Gateway DEPLOYMENT to access Lambda 
+resource "aws_lambda_permission" "apigw" {
+  statement_id  = "AllowAPIGatewayInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.PyLambdaFunction.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/* portion grants access from any method on any resource
+  # within the API Gateway "REST API".
+  source_arn = "${aws_api_gateway_deployment.PyAPIDeployment.execution_arn}/*/*"
+  }
+
+## Permission for API Gateway REST API to access Lambda
+resource "aws_lambda_permission" "lambda_permission" {
+  statement_id  = "AllowTfServerlessPythonAPIInvoke"
+  action        = "lambda:InvokeFunction"
+  function_name = "${aws_lambda_function.PyLambdaFunction.arn}"
+  principal     = "apigateway.amazonaws.com"
+
+  # The /*/*/* part allows invocation from any stage, method and resource path
+  # within API Gateway REST API.
+  source_arn = "${aws_api_gateway_rest_api.PyAPI.execution_arn}/*/*/*"
+  }
+
+output "base_url" {
+  value = "${aws_api_gateway_deployment.PyAPIDeployment.invoke_url}"
+  }
